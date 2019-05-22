bba83259c0f8c443f49431b7a7aea51d4f2be4d8
moshedeitsch <41856538+moshedeitsch@users.noreply.github.com>
2019-05-22 13:03:23 +0300

    Completed  !

---
+++ web/src/components/Lost.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+
+const Lost = () => {
+    return (  
+        <div>
+
+            <h1 style={{color: 'white' }}>
+            You`re Lost! get Back...</h1>
+
+
+            <img style={{paddingTop: '50px' }} src="/images/biohazard.png" alt='404'/>
+        </div>
+    );
+}
+ 
+export default Lost;
